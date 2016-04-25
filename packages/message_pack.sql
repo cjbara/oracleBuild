c319fbf6547877988c5db8d510fa71bc0abc68bb
EC2 Default User <ec2-user@ip-172-31-8-146.us-west-2.compute.internal>
2016-04-25 04:14:56 +0000

    Build the db

--- packages/message_pack.sql
+++ packages/message_pack.sql
@@ -1,5 +1,7 @@
 create or replace package message_pack
 is
+  procedure get_message_conversation(messages OUT sys_refcursor, id1 IN messages.sender_id%type);
+
   procedure send_message(id OUT messages.message_id%type, senderid IN messages.sender_id%type, receiverid IN messages.receiver_id%type, txt IN messages.text%type);
 
   procedure mark_message(id messages.message_id%type, seen messages.read%type);
--- packages/message_pack_body.sql
+++ packages/message_pack_body.sql
@@ -1,5 +1,36 @@
 create or replace package body message_pack
 is
+  procedure get_message_conversation(messages OUT sys_refcursor, id1 IN messages.sender_id%type)
+  is begin
+    open messages for
+    select sender, u.fname||' '||u.lname as receiver, a.sender_id, a.receiver_id, a.text, a.timestamp
+    from
+      (select u.fname||' '||u.lname as sender, a.sender_id, a.receiver_id, a.text, a.timestamp
+      from 
+        (
+        select * from messages
+        where message_id in 
+        (select max(message_id) from (
+        select * from
+          (select m.receiver_id userid, m.sender_id otherid, m.message_id
+          from messages m
+          where m.receiver_id = 1)
+         union
+          (select m.sender_id userid, m.receiver_id otherid, m.message_id
+          from messages m
+          where m.sender_id = 1)
+         )
+         group by otherid
+        )
+    ) a, 
+      users u
+      where a.sender_id = u.user_id
+      ) a,
+    users u
+    where a.receiver_id = u.user_id
+    order by timestamp desc;
+  end;
+
   procedure send_message(id OUT messages.message_id%type, senderid IN messages.sender_id%type, receiverid IN messages.receiver_id%type, txt IN messages.text%type)
   is begin
     insert into messages (sender_id, receiver_id, text)
